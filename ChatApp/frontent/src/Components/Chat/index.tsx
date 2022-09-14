85a9ed01b0644a485acc616183eac467df1214e5
Ronan Wojdyla <ronan.wojdyla@hotmail.fr>
2022-09-14 20:40:47 +0200

    debut projet, serveur en place + react en place

---
+++ ChatApp/frontent/src/Components/Chat/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ChatComponent() {
+    return (
+        <div>
+            Je suis le chat
+        </div>
+    )
+}
+
+export default ChatComponent
---
+++ ChatApp/frontent/src/pages/Chat/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ChatComponent from "../../Components/Chat"
+
+function Chat(){
+    return (
+        <div>
+            yo
+        </div>
+    )
+}
+
+export default Chat 
