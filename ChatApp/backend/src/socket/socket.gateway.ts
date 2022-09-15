8e8dda908a216ab8da64104f13423212c02ca80d
Ronan Wojdyla <ronan.wojdyla@hotmail.fr>
2022-09-15 20:29:10 +0200

    chat fonctionnel avec page html

---
+++ ChatApp/backend/src/socket/socket.gateway.ts
@@ -0,0 +1,28 @@
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'http';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  }
+})
+
+export class SocketGateway {
+
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket){
+    console.log(`Client connected: ${client.id}`)
+  }
+
+  handleDisConnect(client: Socket){
+    console.log(`Client disconnect: ${client.id}`)
+  }
+  @SubscribeMessage('message')
+  handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+    console.log(client.id + " = " + data)
+    this.server.emit('message', client.id, data)
+  }
+}
